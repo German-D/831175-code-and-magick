3750af3384c70b73f0e88a99aa49b40109279844
German Dolnikov <german.dolnikov@ostrovok.ru>
2018-09-11 23:08:27 +0300

    module2-task1

--- js/stat.js
+++ js/stat.js
@@ -10,36 +10,46 @@ var fontGap = 15; // высота строки
 var barHeight = 150; // высота столбца
 var barWidth = 40;
 var nameHeight = cloudHeight;
-var renderCloud = function (ctx, x, y, color) {
+
+
+var renderCloud = function (ctx, shadowX, shadowY, shadowColor, x, y, color) {
+  ctx.fillStyle = shadowColor;
+  ctx.fillRect(shadowX, shadowY, cloudWidth, cloudHeight);
   ctx.fillStyle = color;
   ctx.fillRect(x, y, cloudWidth, cloudHeight);
 };
 
-var renderCongrats = function (ctx, letter, x, y) {
-  ctx.fillText(letter, x, y);
+var renderCongrats = function (ctx, letter1, letter2, x, y1, y2, color, fontsize, font) {
+  ctx.fillStyle = color;
+  ctx.font = fontsize + 'px ' + font;
+  ctx.fillText(letter1, x, y1);
+  ctx.fillText(letter2, x, y2);
+};
+
+var renderColumn = function (ctx, x, y, width, height) {
+  ctx.fillRect(x, y, width, height);
+};
+
+var renderСaption = function (ctx, names, namesX, namesY, times, timesY) {
+  ctx.fillText(names, namesX, namesY);
+  ctx.fillText(times, namesX, timesY);
 };
 
 window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, cloudX + 10, cloudY + 10, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, cloudX, cloudY, '#fff');
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  renderCongrats(ctx, 'Ура, вы победили!', cloudX + fontGap, cloudY + 2 * fontGap);
-  renderCongrats(ctx, 'Список результатов:', cloudX + fontGap, cloudY + 3 * fontGap);
-
-  var maxTime = Math.max.apply(Math, times);
+  var maxTime = Math.max.apply(null, times);
+  renderCloud(ctx, cloudX + 10, cloudY + 10, 'rgba(0, 0, 0, 0.7)', cloudX, cloudY, '#fff');
+  renderCongrats(ctx, 'Ура, вы победили!', 'Список результатов:', cloudX + fontGap, cloudY + 2 * fontGap, cloudY + 3 * fontGap, 'black', 16, 'PT Mono');
 
   names.forEach(function (item, i) {
     var currentColumnX = cloudX + gap + (gap + barWidth) * i;
     var currentColumnY = cloudHeight - fontGap * 2 - barHeight * times[i] / maxTime;
     var currentColumnHeight = barHeight * times[i] / maxTime;
-    var randomNumber = parseFloat(Math.random().toFixed(2));
+    var randomNumber = parseFloat(Math.random().toFixed(2)) + 0.1;
     var randomColor = 'rgba(0, 0, 255, ' + randomNumber + ')';
+    var time = Math.round(times[i]);
 
     ctx.fillStyle = item === 'Вы' ? 'rgba(255, 0, 0, 1)' : randomColor;
-    ctx.fillText(item, currentColumnX, nameHeight);
-    ctx.fillText(Math.round(times[i]), currentColumnX, cloudHeight - 3 * fontGap - currentColumnHeight);
-    ctx.fillRect(currentColumnX, currentColumnY, barWidth, currentColumnHeight);
+    renderСaption(ctx, item, currentColumnX, nameHeight, time, cloudHeight - 3 * fontGap - currentColumnHeight);
+    renderColumn(ctx, currentColumnX, currentColumnY, barWidth, currentColumnHeight);
   });
 };

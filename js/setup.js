886bf367d89f62f48f0fbe9957abce992087b6ef
German Dolnikov <german.dolnikov@ostrovok.ru>
2018-09-13 22:24:58 +0300

    module3-task1

---
+++ js/setup.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var setupBlock = document.querySelector('.overlay');
+setupBlock.classList.remove('hidden');
+var showBlock = document.querySelector('.setup-similar');
+showBlock.classList.remove('hidden');
+
+
+var manyNames = ['Иван', 'Хуан', 'Себастьян', 'Мария', 'Кристоф', 'Юлия', 'Люпита', 'Вашингтон'];
+var manySurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var manyCoatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var manyEyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var manyMages = [{}, {}, {}, {}];
+
+var randomNumber = function (maxNumber) {
+  var result = Math.round(Math.random() * maxNumber);
+  return result;
+};
+
+for (var i = 0; i < manyMages.length; i++) {
+  manyMages[i].name = manyNames[randomNumber(7)];
+  manyMages[i].surname = manySurnames[randomNumber(7)];
+  manyMages[i].coatColor = manyCoatColor[randomNumber(5)];
+  manyMages[i].eyesColor = manyEyesColor[randomNumber(4)];
+}
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var fragment = document.createDocumentFragment();
+for (var j = 0; j < manyMages.length; j++) {
+  fragment.appendChild(renderWizard(manyMages[j]));
+}
+similarListElement.appendChild(fragment);
